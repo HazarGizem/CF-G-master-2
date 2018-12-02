7b59097ec43ed4f2f310f25d2f87ffc6aa45d1a2
Hazar Gizem Peker <gizpek3103@gmail.com>
2018-12-02 13:30:07 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,16 @@
+$('.info').hover(function() {
+  // $(this).find('.info').css("display", "block");
+     $(this).toggleClass('info_hover');
+});
+
+
+// $('.emojis').hover(function() {
+//   // $(this).find('.info').css("display", "block");
+//      $('.info').toggleClass('info_hover');
+// });
+
+
+// $('.emojis').hover(function() {
+//   // $(this).find('.info').css("display", "block");
+//      $(this).toggleClass('emojis_hover');
+// });
